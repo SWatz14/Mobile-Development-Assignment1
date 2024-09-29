3cd71db3b32a98c6c0b88cacc60b59694584d47e
Stanley Watemi <99426536+SWatz14@users.noreply.github.com>
2024-09-28 22:00:40 -0400

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,27 @@
+package com.example.assignment1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Display extends AppCompatActivity {
+    private TextView monthlyPayment;
+    private Button returnButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+        monthlyPayment = findViewById(R.id.monthlyPayment);
+        returnButton = findViewById(R.id.returnButton);
+
+        Intent intent = getIntent();
+        double result = intent.getDoubleExtra("monthlyPayment", 0);
+        monthlyPayment.setText(String.format("Your monthly payment is $ %.2f", result));
+
+        returnButton.setOnClickListener(v -> finish());
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.assignment1;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText principal;
+    private EditText rate;
+    private EditText time;
+    private EditText monthlyPayment;
+    private Button calculate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        principal = findViewById(R.id.principalAmount);
+        rate = findViewById(R.id.interestRate);
+        time = findViewById(R.id.loanTerm);
+        monthlyPayment = findViewById(R.id.monthlyPayment);
+        calculate = findViewById(R.id.button);
+
+        calculate.setOnClickListener(v -> {
+            double amount = Double.parseDouble(principal.getText().toString());
+            double percent = Double.parseDouble(rate.getText().toString());
+            double period = Double.parseDouble(time.getText().toString());
+            double result = amount * (percent / 1200) * Math.pow(1 + percent / 1200, period)
+                    / (Math.pow(1 + percent / 1200, period) - 1);
+            monthlyPayment.setText(String.format(" $ %.2f", result));
+
+            Intent intent = new Intent(MainActivity.this, Display.class);
+            intent.putExtra("monthlyPayment", result);
+            startActivity(intent);
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

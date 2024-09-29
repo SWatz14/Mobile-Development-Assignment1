c1c08edfe3a714aa12a9aa04f2cd724dcffec711
Stanley Watemi <99426536+SWatz14@users.noreply.github.com>
2024-09-28 22:07:15 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText monthlyPayment;
     private Button calculate;
 
-    @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
